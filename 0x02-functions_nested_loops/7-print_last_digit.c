f9c9616cb4b2df4f4c8b40d49cadecd6cfb090de
SudoAlex-16 <olaglo1616@gmail.com>
2022-09-14 18:36:19 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,18 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0 || n == 0)
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
 	{
-		_putchar(n % 10 + '0');
-		return (n % 10);
+		last_digit = -(last_digit);
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
 	else
 	{
-		n = n * -1;
-		_putchar(n % 10 + '0');
-		return (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit % 10);
 	}
+	return (0);
 }
