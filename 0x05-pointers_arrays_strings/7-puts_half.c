682aea69d616eb3a48464b878323d4fa9d991682
SudoAlex-16 <olaglo1616@gmail.com>
2022-09-20 05:47:44 -0700

    correction on 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,26 +8,14 @@
 void puts_half(char *str)
 {
 	int c, a;
-	char l;
 
-	for (c = 0; str[c] != 0; c++)
+	for (c = 0; str[c] != '\0'; ++c)
 	{
+		;
 	}
-	if ((c % 2) == 0)
+	for (a = (c + 1) / 2; a < c; ++a)
 	{
-		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l =  str[a];
-			_putchar(l);
-		}
-	}
-	else
-	{
-		for ((a = (c - 1) / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
+		_putchar(str[a]);
 	}
 	_putchar('\n');
 }
