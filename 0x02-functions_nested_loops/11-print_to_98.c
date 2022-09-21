1bf2a675fa4fafa17e095e3d2abb11366ebe0f2d
SudoAlex-16 <olaglo1616@gmail.com>
2022-09-21 12:27:18 -0700

    correction for 9 and 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,19 @@
 
 void print_to_98(int n)
 {
+	while (n != 98)
+	{
+		printf("%d", n);
+		if (n > 98)
+		{
+			--n;
+		}
+		else if (n < 98)
+		{
+			++n;
+		}										printf(", ");
 
-	if (n < 98)
-	{
-		for (n = n; n < 98; n++)
-		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
-	}
-	else
-	{
-		for (n =n; n > 98; n--)
-		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
-	}
+												}
+		printf("%d", 98);
+		printf("\n");
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,31 @@
  */
 
 void times_table(void)
-
 {
-	int num, mult, prod;
+	int i, j, product;
 
-	for (num = 0; num <= 9; ++num)
+	for (i = 0; i <= 9; ++i)
 	{
-		_putchar(48);
-		for (mult = 1; mult <= 9; ++mult)
-
+		for (j = 0; j <= 9; ++j)
 		{
-			_putchar(',');
-			_putchar(' ');
-			prod = num * mult;
-			if (prod <= 9)
+			product = i * j;
+			if (j != 0)
 			{
+				_putchar(',');
 				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+				}
+			}
+			if (product <= 9)
+			{
+				_putchar('0' + product);
 			}
 			else
 			{
-				_putchar((prod / 10) +  48);
-				_putchar((prod % 10) + 48);
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 		}
 		_putchar('\n');
